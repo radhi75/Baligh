4223a5cd33542f904d5ccd66552eceeade75f8c6
radhi aissa <96472173+radhi75@users.noreply.github.com>
2024-09-13 10:48:13 +0000

    msg

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,29 +1,35 @@
-import React from "react"
-import Heading from "../../common/heading/Heading"
-import "./Hero.css"
+import React from "react";
+import Heading from "../../common/heading/Heading";
+import "./Hero.css";
 
 const Hero = () => {
   return (
     <>
-      <section className='hero'>
-        <div className='container'>
-          <div className='row'>
-            <Heading subtitle='WELCOME TO ACADEMIA' title='Best Education Expertise' />
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-            <div className='button'>
-              <button className='primary-btn'>
-                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+      <section className="hero">
+        <div className="container">
+          <div className="row">
+            <Heading
+              subtitle="WELCOME TO Librairie khawarezmi"
+              title="Best Education Expertise"
+            />
+            <p>
+              Far far away, behind the word mountains, far from the countries
+              Vokalia and Consonantia, there live the blind texts.
+            </p>
+            <div className="button">
+              <button className="primary-btn">
+                SIGN IN <i className="fa fa-long-arrow-alt-right"></i>
               </button>
               <button>
-                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+                SIGN UP <i className="fa fa-long-arrow-alt-right"></i>
               </button>
             </div>
           </div>
         </div>
       </section>
-      <div className='margin'></div>
+      <div className="margin"></div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
